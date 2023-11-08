6d56e5f802529ee5411eeb337e3a3640ffb20138
myerbro3 <you@example.com>
2023-11-07 22:44:57 -0700

    Week 11 HW

---
+++ Week_11/BM_MarkedAnalysis.R
@@ -0,0 +1,392 @@
+#######################################################################
+#######################################################################
+##     This script was created by Dr. Jen Cruz as part of            ##
+##            the Applied Population Ecology Class                  ###
+##                                                                   ##  
+## Here we import our cleaned data for single season capture-        ##
+#  recapture analysis for Piute ground squirrels at the NCA.         ##
+##                                                                   ##
+## We start with analysis in unmarked. We will run 3 model options   ##
+## M[0] = intercept only model for detection.                         # 
+# M[t] = survey-varying detection model                              ##
+# M[b] = behavior-effect on detection. To allow for trap-happiness or #
+# trap-shyness following initial capture.                             #
+#######################################################################
+
+##### Set up your workspace and load relevant packages -----------
+# install packages
+# install.packages( "AHMbook" )
+#Now load relevant packages:
+library( tidyverse )
+library( unmarked ) 
+library( AHMbook ) #contains data and functions from book
+
+## end of package load ###############
+###################################################################
+#### Load or create data -----------------------------------------
+# Clean your workspace to reset your R environment. #
+rm( list = ls() )
+# Check that you are in the right project folder
+getwd()
+# load workspace with clean data
+load( "MarkPrepWorkspace.RData")
+
+#### End of data load -------------
+####################################################################
+##### Ready data for analysis --------------
+
+# We need to custom build cell probability functions for each model type. 
+# We start with M[t]
+# Remember that custom crPiFun can only take a single argument p, which must be#
+# the I x J matrix of detection probabilities. 
+# we define the probabilities for each possible capture history:
+crPiFun.t <- function(p) {
+  p1 <- p[,1] #detection for J survey 1
+  p2 <- p[,2] #detection for J survey 2
+  p3 <- p[,3] #detection for J survey 3
+  cbind("001" = (1-p1) * (1-p2) * p3,
+        "010" = (1-p1) * p2 * (1-p3),
+        "011" = (1-p1) * p2 * p3,
+        "100" = p1 * (1-p2) * (1-p3),
+        "101" = p1 * (1-p2) * p3,
+        "110" = p1 * p2 * (1-p3),
+        "111" = p1 * p2 * p3)
+}
+
+# When providing a user defined piFun we also need information on how #
+# to handle missing values by supplying a matrix of zeros (if not sampled) and ones (if sampled) #
+# with rows = J (repeat surveys) and  cols = observed capture histories (ch):
+o2y <- matrix( data = 1, nrow = J, ncol = CH )
+
+# For the M[t] and M[0] models we can use our observations pooled #
+# by capture histories. 
+# A restriction of unmarked is that we cannot include #
+# sites with all zero captures. i.e. sites where we trapped but #
+# we caught no animals. We therefore work out which rows from our #
+# dataframes we need to keep
+keep <- which( rowSums(y_ik ) > 0 )
+#define unmarked dataframe for M[t] and M[o] for single season
+umf.Mt <- unmarkedFrameGMM( y = y_ik[keep,],
+              #define abundance covariates
+              siteCovs = as.data.frame(ik_sc[keep,]),
+              #define observation covariates:
+              obsCovs = list( wind = ij_wide[keep,widx],
+                              temp = ij_wide[keep,tidx],
+                              effort =  ij_wide[keep,eidx] ),
+              #we use the pifun we defined for M[t]:
+              obsToY = o2y, piFun = "crPiFun.t", 
+              numPrimary = 1 )
+#What does it look like?
+summary(umf.Mt)
+
+#Now define cell probabilities for the behavioral model M[b]:
+#note this model assumes that on first capture, the animals are 'naive' #
+# to the capture process. However, after being captured once, their #
+# reaction to the capture process changes. 
+crPiFun.b <- function(p) { 
+  pNaive <- p[,1]
+  pWise <- p[,2]
+  cbind("001" = (1-pNaive) * (1-pNaive) * pNaive,
+        "010" = (1-pNaive) * pNaive * (1-pWise),
+        "011" = (1-pNaive) * pNaive * pWise,
+        "100" = pNaive * (1-pWise) * (1-pWise),
+        "101" = pNaive * (1-pWise) * pWise,
+        "110" = pNaive * pWise * (1-pWise),
+        "111" = pNaive * pWise * pWise)
+}
+
+### For the M[b] model we create a behavior dummy variable 
+bMat <- matrix( c("Naive", "Wise", "Wise"), dim(y_ik[keep,])[1], 
+                J, byrow = TRUE )
+# Remember that there are limitations to the models that you can fit #
+# for example, you cannot fit an M[b,t] model 
+
+#define unmarked dataframe for M[b]
+umf.Mb <- unmarkedFrameGMM( y = y_ik[keep,],
+              #define abundance covariates
+              siteCovs = as.data.frame(ik_sc[keep,]),
+              #define observation covariates:
+              obsCovs = list( behavior = bMat ),
+              obsToY = o2y, piFun = "crPiFun.b", 
+              numPrimary = 1 )
+#check
+summary(umf.Mb)
+### end data prep -----------
+#######################################################################
+### Analyze data ------------------------------------------
+# We are now ready to perform our analysis.
+# For a single season: ######################
+# We start with M[o] intercept only model to check it all runs: -------
+M0.P <- gmultmix( #first function is abundance model. 
+  #Second is availability. Third is detection.
+            #we start with intercept only models
+            ~1, ~1, ~1, 
+          #we can provide the Mt dataframe
+          umf.Mt, engine="R")
+#Did it work?
+M0.P
+# AIC: -35.70343 
+# We adjust to a Negative Binomial:
+M0.NB <- gmultmix(   ~1, ~1, ~1, 
+  umf.Mt, 
+  mixture = "NB",
+  engine="R" )
+#Did it work?
+M0.NB
+# AIC: -306.079 
+#Now we run the full Mt model including also abundance predictors:
+Mt.full.P  <- gmultmix( ~ 1 + shrub,
+                ~ 1 , 
+                ~ 1 + wind + temp + effort,
+                umf.Mt, 
+                mixture = "P",
+                engine="R" )
+
+#Did it work?
+Mt.full.P
+# AIC: -174.7097  
+#What do results say at first glance?
+#Answer: THE NEGATIVE BINOMIAL MODEL APPEARS TO PERFORM MUCH BETTER THAN THE OTHER 
+# MODELS. HOWEVER IN CLASS WE DISCUSSED THAT THIS MODEL MAY BE BIASED DUE TO HOW IT 
+# INTERPRETS OVERDISPERSION VS DETECTION. ESPECIALLY WITH ONLY 20 SITES. 
+#
+# We test whether the Negative Binomial as an alternative distribution
+Mt.full.NB  <- gmultmix(  ~ 1 + shrub,
+                         ~ 1 , 
+                         ~ 1 + wind + temp + effort,
+                        umf.Mt, 
+                        mixture = "NB",
+                        engine="R" )
+
+#check
+Mt.full.NB
+# AIC: -310.3774 
+# We compare against the two full behavioral models
+#The first uses the Poisson distribution:
+Mb.full.P  <- gmultmix(  ~ 1 + shrub,
+                          ~ 1 , 
+                          ~ 1 + behavior,
+                          umf.Mb, 
+                          mixture = "P",
+                          engine="R" )
+Mb.full.P
+# AIC: -172.5878 
+#The second full model uses the Negative Binomial:
+Mb.full.NB  <- gmultmix(  ~ 1 + shrub,
+                          ~ 1 , 
+                          ~ 1 + behavior,
+                          umf.Mb, 
+                          mixture = "NB",
+                          engine="R" )
+
+Mb.full.NB
+# AIC: -313.9206 
+# We therefore move on and try to see which of our Mt, Mo or Mb models #
+# provided a better fit for our data: 
+#Create model list:
+Mtmodels <- fitList( "M0.NB" = M0.NB, 
+                     "Mt.P" = Mt.full.P, 
+                      "Mt.NB" = Mt.full.NB 
+                       )
+
+#compare
+modSel( Mtmodels )
+# Which is our top model?
+# Answer: THE MT.NB AND M0.NB MODELS BOTH OUTPERFORMED THE POISSON MODEL. 
+# HOWEVER ADDING THE BEHAVIOR EFFECTS IN THE MT.NB MODEL DO NOT SIGNIFICANTLY 
+# INMPROVE THE AIC VALUES. 
+# 
+# What does it suggest: 
+# Answer: BASED ON THIS, IT APPEARS THE M0.NB MODEL IS THE "BEST" MODEL
+#
+#Now repeat for Mb models
+Mbmodels <- fitList( "M0.NB" = M0.NB, 
+                     "Mb.P" = Mt.full.P, 
+                     "Mb.NB" = Mt.full.NB 
+)
+modSel( Mbmodels )
+##########################################################################
+# Model fit and evaluation -----------------------------------------------
+# We rely on unmarked options for boostrap goodness-of-fit option in #
+# combination with custom built function fitstats, from Kery and Royle's
+# Hierarchical modelling book, Chpt 7 pg 333. Also see example in ?parboot.
+# The function computes error sum-of-squares, standard Chi-square and the #
+# Freeman-Tukey statistic:
+# Function returning three fit-statistics.
+# fitstats <- function(fm) {
+#   observed <- getY(fm@data)
+#   expected <- fitted(fm)
+#   resids <- residuals(fm)
+#   sse <- sum(resids^2)
+#   chisq <- sum((observed - expected)^2 / expected)
+#   freeTuke <- sum((sqrt(observed) - sqrt(expected))^2)
+#   out <- c(SSE=sse, Chisq=chisq, freemanTukey=freeTuke)
+#   return(out)
+# }
+# the fitstats function is also available via the AHMbook package
+# so we use that:
+(gof.Mt.full.NB <- parboot( Mt.full.NB, fitstats2, nsim = 100) )
+
+(gof.Mt.full.P <- parboot( Mt.full.P, fitstats2, nsim = 100) )
+
+(gof.Mb.full.NB <- parboot( Mb.full.NB, fitstats2, nsim = 100) )
+
+(gof.Mb.full.P <- parboot( Mb.full.P, fitstats2, nsim = 100) )
+
+# What do these results suggest? 
+# Answer: 
+# MT.FULL.NB = GAVE THE ERROR "Model fitting failed in 8 sims. "
+# HOWEVER IT PASSED 3 OF 6 STATISTICS ACCORDING TO THE PVALUES
+
+#MT.FULL.P = GAVE ERRORS "NANS" PRODUCED
+# FAILED 6 OF 6 
+
+# MB.FULL.NB = GAVE THE ERROR "Model fitting failed in 14 sims. "
+# HOWEVER PASSED 5 OF 6 
+
+# MB.FULL.P =  NO ERRORS BUT FAILED 6 OF 6 MEANING IT FAILED SUCCESSFULLY? 
+
+# MOVING FORWARD WITH EITHER MB.FULL.NB 
+# OR MT.FULL.NB, WHICHEVER DOES NOT FAIL WHEN MAKING PREDICTIONS...
+
+
+#########################################################################
+##### Summarizing model output ##############
+# Select model to use
+fm <- MB.full.NB
+#Extract abundance estimates from top model
+#start by creating a dataframe to hold results
+outdf <- ik_df %>% select( idno, id, SiteID, year ) %>% 
+        dplyr::filter( idno %in% rownames(y_ik[keep,]) )
+#now calculate the mean and 95%CIs using the ranef function
+outdf <- cbind( outdf, bup(ranef( fm)),confint( ranef( fm)))
+colnames(outdf)[5:7] <- c( "Mean", "Lower", "Upper")
+#Add raw counts
+outdf$rawtrap <- as.vector(rowSums( y_ik[keep,] ))
+#check
+head(outdf)
+#plot
+ggplot( outdf, aes( x = as.factor(SiteID), y = Mean ) ) +
+  theme_classic(base_size = 15) +
+  geom_point( size = 3 ) +
+  # add confidence intervals
+  geom_errorbar( aes(ymin = Lower, ymax = Upper ), 
+                 size = 1.5, width = 0.3 )  +
+  facet_wrap( ~year, ncol = 1 )
+
+
+# Estimate partial prediction plots for predictors with 95% CIs not overlapping zero:
+# how many values do we use:
+n <- 100
+# Use the observed values to define our range:
+shrub <- seq( min( ik_df[,"shrub"]), max( ik_df[,"shrub"]),
+                   length.out = n )
+#scale
+shrub.std <- scale( shrub )
+#combine standardized predictors into anew dataframe to predict partial relationship
+# for abundance submodel:
+abundData <- data.frame( shrub = shrub.std, annual = 0, 
+                         perennial = 0  )
+#predict partial relationship:
+pred.shrub <- predict( fm, type = "lambda", newdata = abundData, 
+                       appendData = TRUE )
+#view
+head( pred.shrub ); dim( pred.shrub )
+
+# create plot for ecological submodel:
+shrubp <- cbind( pred.shrub[,c("Predicted", "lower", "upper") ], shrub ) %>%
+  # define x and y values
+  ggplot(., aes( x = shrub, y = Predicted ) ) + 
+  #choose preset look
+  theme_bw( base_size = 15 ) +
+  # add labels
+  labs( x = "Shrub cover (%)", y = "Relative abundance" ) +
+  # add band of confidence intervals
+  geom_smooth( aes(ymin = lower, ymax = upper ), 
+               stat = "identity",
+               size = 1.5, alpha = 0.5, color = "grey" ) +
+  # add mean line on top
+  geom_line( size = 2 ) 
+#view
+shrubp
+# How do you interpret this relationship?
+# Answer: THERE APPEARS TO BE A NEGATIVE RELATIONSHIP BETWEEN SHRUB COVER
+# AND RELATIVE ABUNDANCE, UNLIKE THE SIMULATED DATA WHICH SHOWS THE OPPOSITE. 
+
+#Compare abundance and partial prediction plot for the 
+# model with the NB distribution instead. #
+fm <- Mt.full.NB
+#Extract abundance estimates from top model
+#start by creating a dataframe to hold results
+outdf <- ik_df %>% select( idno, id, SiteID, year ) %>% 
+  dplyr::filter( idno %in% rownames(y_ik[keep,]) )
+#now calculate the mean and 95%CIs using the ranef function
+outdf <- cbind( outdf, bup(ranef( fm)),confint( ranef( fm)))
+colnames(outdf)[5:7] <- c( "Mean", "Lower", "Upper")
+#Add raw counts
+outdf$rawtrap <- as.vector(rowSums( y_ik[keep,] ))
+#check
+head(outdf)
+#plot
+ggplot( outdf, aes( x = as.factor(SiteID), y = Mean ) ) +
+  theme_classic(base_size = 15) +
+  geom_point( size = 3 ) +
+  # add confidence intervals
+  geom_errorbar( aes(ymin = Lower, ymax = Upper ), 
+                 size = 1.5, width = 0.3 )  +
+  facet_wrap( ~year, ncol = 1 )
+
+
+# Estimate partial prediction plots for predictors with 95% CIs not overlapping zero:
+# how many values do we use:
+n <- 100
+# Use the observed values to define our range:
+shrub <- seq( min( ik_df[,"shrub"]), max( ik_df[,"shrub"]),
+              length.out = n )
+#scale
+shrub.std <- scale( shrub )
+#combine standardized predictors into anew dataframe to predict partial relationship
+# for abundance submodel:
+abundData <- data.frame( shrub = shrub.std, annual = 0, 
+                         perennial = 0  )
+#predict partial relationship:
+pred.shrub <- predict( fm, type = "lambda", newdata = abundData, 
+                       appendData = TRUE )
+#view
+head( pred.shrub ); dim( pred.shrub )
+
+# create plot for ecological submodel:
+shrubp <- cbind( pred.shrub[,c("Predicted", "lower", "upper") ], shrub ) %>%
+  # define x and y values
+  ggplot(., aes( x = shrub, y = Predicted ) ) + 
+  #choose preset look
+  theme_bw( base_size = 15 ) +
+  # add labels
+  labs( x = "Shrub cover (%)", y = "Relative abundance" ) +
+  # add band of confidence intervals
+  geom_smooth( aes(ymin = lower, ymax = upper ), 
+               stat = "identity",
+               size = 1.5, alpha = 0.5, color = "grey" ) +
+  # add mean line on top
+  geom_line( size = 2 ) 
+#view
+shrubp
+# What differences do you see? 
+# BOTH PARTIAL DISTRIBUTION PLOTS SHOW A DECREASE IN ABUNDANCE WITH INCREASE IN 
+# SHRUB COVER. HOWEVER THE MT MODEL HAS MUCH LARGER CONFIDENCE INTERVALS THAN
+# THE MB MODEL
+# What distribution would you choose moving forward? # 
+# Answer: BASED ON THIS ANALYSIS I WOULD CHOSE THE MB MODEL. HOWEVER WITH SO MUCH 
+# CORRELATION BETWEEN THE VARIABLES (DESCRIBED ABOVE) AND NOT FULLY UNDERSTANDING
+# HOW THE NB MODEL TYPE IS HANDING THE OVERDISPERSION, I WOULD NEED TO DO MUCH 
+# MORE RESEARCH TO BE SURE THIS IS RIGHT MODEL
+
+###################################################################
+############################################################################
+################## Save your data and workspace ###################
+# Save workspace:
+save.image( "MarkedResults.RData" )
+
+########## End of saving section ##################################
+
+############# END OF SCRIPT #####################################
---
+++ Week_11/BM_MarkedPrep.R
@@ -0,0 +1,239 @@
+#######################################################################
+#######################################################################
+####   This script was created by Dr. Jen Cruz as part of      ########
+####          the Applied Population Ecology Class              #######
+####                                                           ########
+# Our study species is the Piute ground squirrel, Spermophilus        # 
+# mollis at the Morley Nelson Birds of Prey Nature Conservation Area. #
+#                                                                     #            
+# We shift to real capture-recapture data from 14 sites collected     #
+# during 2021 to 2023. Note that different sites were surveyed each year#
+#                                                                     #
+# Detection predictors: 1-effort: number of hours the traps were open #
+# each day, 2-mean tempC_st up to the hour the traps were closed, 3-mean #
+# wind up to the hour the traps were closed each day                   #
+#
+# Predictors for abundance: only 20 sitesXyear so be mindful of how            #
+# many we can reasonably include. The data this time comes from RAP:
+# https://rangelands.app/products/ #
+# and includes measures of cover for: shrub, perennial, annual and the #
+# sum of annual and perennial = herbaceous. Cover is for the year prior# # 
+#
+##############################################################################################################################################
+##### Set up your workspace and load relevant packages -----------
+# Clean your workspace to reset your R environment. #
+rm( list = ls() )
+# Check that you are in the right project folder
+getwd()
+
+# load packages:
+library( tidyverse ) 
+## end of package load ###############
+###################################################################
+#### Load or create data -----------------------------------------
+# set directory where your data are. 
+# I have it in a Data folder in my Rstudio project:
+datadir <- paste( getwd(), "/Data/", sep = "" )
+
+#import individual level capture histories 
+ch <- read.csv( paste0(datadir, "trapdata21-23wide.csv" ),
+                header = TRUE)
+#import raw ikj predictors
+ikj_df <- read.csv( paste0( datadir, "traps_ikj_df_21-23.csv"),
+                   header = TRUE )
+#import iXk predictors
+ik_df <- read.csv( paste0( datadir, "traps_ik_df_21-23.csv"),
+                   header = TRUE )
+
+########## end of data load ################################
+#######################################################################
+######## explore individual capture data #############
+# Start viewing our observations dataframe:
+head( ch ); dim( ch )
+
+###define model parameters:
+#how many sites?
+(S <- length( unique(ch$SiteID) ) )
+#how many unique siteXyear combo?
+I <- max( ik_df$idno )
+#which years (primary seasons) were sampled:
+(yrrange <- sort( unique( ch$year ) ) )
+#how many years were sampled:
+(T <- length( yrrange ) )
+#max number of surveys at a given site
+J <- 3
+#number of unique individuals
+N <- max( ch$AnimalID, na.rm = TRUE )
+#number of individuals (including augmented group)
+M <- N * 3
+
+#calculate naive abundance for each sex:
+N_naive <- ch %>% group_by( id, year, Sex ) %>% 
+  summarise( captures = n()  ) %>% ungroup()
+#check
+head( N_naive ); dim(N_naive )
+#plot naive estimates:
+ggplot( N_naive, aes( x = as.factor(year), y = captures,
+                      color = as.factor(id)) ) +
+  geom_point( size = 2 ) +
+  theme_bw( base_size = 15) +
+  theme( legend.position = "none" ) +
+  facet_wrap( ~Sex, nrow = 2 )
+# Can we see any sex differences?
+# Answer: SEX RATIO VARIES BETWEEN SITES; SOME HAVE MORE MALES, SOME MORE FEMALES
+#
+
+# Plot naive annual estimates of abundance
+N_naive %>% group_by( id ) %>% 
+  summarise( total = sum(captures),
+             year = first(year)) %>% 
+  ggplot(., aes( x = as.factor(year), y = total,
+                 color = as.factor(id) ) ) +
+  geom_point( size = 2 ) +
+  theme_bw( base_size = 15 )
+#What does this tell us?
+# Answer: IN GENERAL, DETECTION DECLINED, BUT IN THE CASE OF SDHPLA4 IT INCREASED
+# VARIABILITY BETWEEN DETECTION IN SITES BETWEEN YEARS 
+#
+# we can also summarize capture histories
+
+# Answer: 
+#
+head(ch)
+
+#create collapsed capture history column 
+ch$ch  <- apply( ch[,c('j_1', 'j_2', 'j_3') ], 
+                 1, paste, collapse="" )
+
+#turn ch to factor for multi-season:
+ch$ch  <- factor( ch$ch, 
+                  levels=c("001", "010", "011", "100", "101", "110", "111"))
+
+#number of observed capture histories:
+CH <- length(levels(ch$ch))
+#create tally of individuals captured under each capture history
+y_ik <- table( ch$idno, ch$ch ) 
+class(y_ik)
+#turn to dataframe
+y_ik <- as.data.frame.matrix(y_ik)
+#add idno column so that we can combine with ik_df
+y_ik$idno <- as.numeric( rownames(y_ik) )
+#now add sites with non of those capture histories (no captures)
+# extracted from ik_df
+y_ik <- ik_df %>% 
+  dplyr::select( idno ) %>% 
+  left_join( y_ik, by = "idno" ) %>% 
+  #remove id column since we don't need it anymore
+  dplyr::select( -idno )
+# replace NA with 0s
+y_ik[is.na(y_ik)] <- 0 
+#view
+y_ik
+#calculate number of individuals captured per site
+n_ik <- apply( y_ik, 1, sum )
+
+table( ch$ch )
+# what do you notice? 
+# MANY INDIVIDUALS CAPTURED ON FIRST DAY ONLY
+# NOT AS MANY CAPTURED ON FIRST OR SECOND DAY ONLY
+# FEW CAPTURED ON SECOND AND THIRD, FIRST AND SECOND, OR ALL THREE DAYS
+
+##### prepare data for data augmentation analysis #####
+# instead of summarizing to capture histories we keep information 
+# for each captured individual: 
+#extract id for sitesXyear with captures
+capsites <-  unique( ch$idno)
+#now add rows for sites with all 0 captures
+y <- ik_df %>% filter( !(idno  %in% capsites ) ) %>% 
+  dplyr::select( idno, id, SiteID, year  )
+#append rows for sites wiht no captures to ch 
+y <- bind_rows( ch, y )
+#augment individual dataframe with M individuals
+y <- rbind( y, matrix(NA, nrow = M-N, ncol = NCOL(ch), 
+                      dimnames = list(NULL, colnames(ch))))
+
+#need to replace NAs with all zero captures
+y$j_1[ is.na( y$j_1 ) ] <- 0
+y$j_2[ is.na( y$j_2 ) ] <- 0
+y$j_3[ is.na( y$j_3 ) ] <- 0
+
+tail(y)
+#check sites got added
+unique(y$idno)
+#note that the trapped sites with zero captures are added at the top
+#recalculate M since we added extra rows for sites with no captures
+M <- dim(y)[1] #- N #M - N = number of augmented individuals
+
+############### prepare predictor data ##############
+#define predictors for detection
+preds <- c("effort_mins", "tempC_st", "wind_kmph_st" )
+#view histograms and correlation
+for( n in preds){
+  hist( ikj_df[,n], main = n )
+}
+#check correlation
+cor(ikj_df[,preds])
+# what do these two check tell us? are we ok moving forward?
+# answer: THERE DOES NOT APPEAR TO BE ANY SIGNIFICANT CORRELATION BETWEEN THE VARIABLES
+#
+
+#Now we are ready to scale predictors
+ij_sc <- ikj_df
+#scale
+for( i in preds ){
+  ij_sc[ ,i ] <- scale( ij_sc[ ,i ] ) 
+}
+#check
+head( ij_sc )
+#turn to wide format
+ij_wide <- ij_sc %>%
+  pivot_wider( names_from = Survey,
+     values_from = c( jday, effort_mins, tempC_st, wind_kmph_st),
+     values_fill = 0 ) %>% 
+  dplyr::arrange( SiteID, year )
+
+# extract column ids for each detection predictor
+widx <- grep( "wind", colnames( ij_wide ), value = FALSE)
+tidx <- grep( "temp", colnames( ij_wide ), value = FALSE)
+eidx <- grep( "effort", colnames( ij_wide ), value = FALSE)
+
+head( ij_wide)
+
+###for abundance predictors now plot histograms 
+# and calculate correlation between predictors ###
+## here:
+
+abund <- c("shrub", "perennial", "annual", "herbaceous")
+#view histograms and correlation
+for( n in abund){
+  hist( ik_df[,n], main = n )
+}
+#check correlation
+cor(ik_df[,abund])
+#
+# From this comment on which predictors you are choosing to use 
+# moving forward? 
+# Answer: THERE IS HIGH CORRELATION BETWEEN ALL PREDICTORS
+#               shrub      perennial  annual      herbaceous
+# shrub       1.0000000 -0.6652996 -0.8015314 -0.8598714
+# perennial  -0.6652996  1.0000000  0.4835424  0.8149528
+# annual     -0.8015314  0.4835424  1.0000000  0.9013366
+# herbaceous -0.8598714  0.8149528  0.9013366  1.0000000
+#
+# SINCE WE KNOW SHRUB INCLUDES SAGEBRUSH, I WILL PROBABLY ONLY CHOOSE 
+# SHRUB. MAY ADJUST LATER...
+
+#now we are ready to scale predictors for abundance
+ik_sc <- apply( ik_df[, c("shrub", "perennial", "annual", "herbaceous")],
+                2, scale )
+#check
+head( ik_sc)
+
+##########    save relevant data and workspaces     ###########
+# Since we created multiple objects that we want to use in our analysis #
+# we save the entire workspace and load it into the analysis script #
+save.image( "MarkPrepWorkspace.RData" )
+########## End of saving section ##################################
+################## Save your data and workspace ###################
+
+############# END OF SCRIPT ########################################
